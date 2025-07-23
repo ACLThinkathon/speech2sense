2a943018cfed2052bd83a1a3d6a04f4bf3c36e4b
Shweta.I <sinamdar@logitech.com>
2025-07-23 17:27:17 +0530

    Added output file and made some changes code

--- audio_to_text.py
+++ audio_to_text.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import argparse
 from mutagen import File
+from mutagen.mp4 import MP4
 from mutagen.wave import WAVE
 from mutagen.mp3 import MP3
 import eyed3.mp3.headers as hdr
@@ -17,6 +18,7 @@ output_text_file = "data/output_file.txt"
 
 def rebuild_audio(input_path: str, output_path: str) -> None:
     """
+    This method can be used to convert mp3 file to wav file.
     Uses FFmpeg to convert a potentially malformed MP3 into a clean WAV file.
     This ensures no corrupt frames disrupt downstream processing.
     """
@@ -35,6 +37,7 @@ def rebuild_audio(input_path: str, output_path: str) -> None:
 
 def main(audio_wav_file_name):
     # Transcribe audio with timestamped segments
+    # If translation from any language to English is required, use translations instead of transcriptions
     with open(audio_wav_file_name, "rb") as f:
         resp = client.audio.transcriptions.create(
             file=f,
@@ -71,7 +74,7 @@ def main(audio_wav_file_name):
         })
 
     # Map speaker IDs to friendly names
-    mapping = {"SPEAKER_00": "Speaker 1", "SPEAKER_01": "Speaker 2"}
+    mapping = {"SPEAKER_00": "Agent", "SPEAKER_01": "Customer"}
     for m in merged:
         m["speaker_name"] = mapping.get(m["speaker"], m["speaker"])
 
@@ -95,15 +98,7 @@ if __name__ == "__main__":
     print("Input Filename: ", file_name)
 
     audio = File(file_name)
-    if isinstance(audio, MP3):
-        print("Filetype is mp3")
-        rebuild_audio(file_name, clean_audio_wav_file)
-        main(clean_audio_wav_file)
-    elif isinstance(audio, WAVE):
-        print("Filetype is wav")
+    if isinstance(audio, (MP3, WAVE, MP4)) or has_mp3_frame(file_name):
         main(file_name)
-    elif has_mp3_frame(file_name):
-        print("MP3 frames found but Mutagen couldn’t parse them — still processing as MP3")
-        rebuild_audio(file_name, clean_audio_wav_file)
     else:
         print("Unsupported or unrecognized type")
