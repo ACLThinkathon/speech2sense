a900ee97b36125a5e52c08610d4fa575e85f53c8
Priyanka Gupte <pgupte@everchron.com>
2025-07-28 15:39:53 +0530

    updated sleep time

--- start_all.bat
+++ start_all.bat
@@ -2,7 +2,7 @@
 echo Starting FastAPI backend on port 8000...
 start cmd /k "cd api && uvicorn main:app --host 0.0.0.0 --port 8000 --reload"
 
-timeout /t 5
+timeout /t 30
 
 echo Launching Streamlit frontend...
 cd webui
